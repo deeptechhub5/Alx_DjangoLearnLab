557ac0078bc3af7fb56571cdbcdebebf211a3557
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-11-06 03:07:06 +0000

    Django Views and URL Configuration

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),  # function-based view
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # class-based view
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),  # include app URLs
+]
