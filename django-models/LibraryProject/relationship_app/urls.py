e5e3ce25b998c11a7e83af71e0e87777a78f3531
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-11-09 05:22:57 +0000

    changes made to views.py in relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from .views import login_view, logout_view, register_view
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # class-based view
     path('admin/', admin.site.urls),
     path('', include('relationship_app.urls')),  # include app URLs
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Library, Book
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+
 
 # Function-based view to list all books
 def list_books(request):
@@ -12,3 +16,31 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# Register view
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+# Login view
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # You can change this to a page you have
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+# Logout view
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
