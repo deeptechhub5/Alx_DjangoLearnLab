734753544fa89af69de636758e441b6e709834cb
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-11-16 14:43:03 +0000

    done now

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
     list_filter = ('publication_year',)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password", "date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "date_of_birth", "profile_photo", "is_staff", "is_active"),
+        }),
+    )
+
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
 class Book(models.Model):
@@ -8,3 +9,36 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = None   # remove username
+    email = models.EmailField(unique=True)
+
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
