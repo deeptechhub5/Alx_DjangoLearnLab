230a93c7fff7026b1fc0e3211188a6dc8a1a28ad
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-11-25 14:25:11 +0000

    initiated a Django project and used advanced API development with Django REST Framework

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Author, Book
+from datetime import datetime
+
+
+class BookSerializer(serializers.ModelSerializer):
+
+    # Custom validation: prevent future publication years
+    def validate_publication_year(self, value):
+        current_year = datetime.now().year
+        if value > current_year:
+            raise serializers.ValidationError(
+                "Publication year cannot be in the future."
+            )
+        return value
+
+    class Meta:
+        model = Book
+        fields = "__all__"
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    # Nested books → show all books written by the author
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ["name", "books"]
