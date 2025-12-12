f784b763c07118ccfb7d3a882c85a1a1c5f6bd58
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-12-12 06:53:04 +0000

    Added posts and comments functionality with pagination and filtering

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,33 @@
+from rest_framework import viewsets, permissions, filters
+from rest_framework.pagination import PageNumberPagination
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsAuthorOrReadOnly
+
+
+class StandardPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    pagination_class = StandardPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    pagination_class = StandardPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
     # Local apps
     'accounts',
+    'posts', 
 ]
 
 MIDDLEWARE = [
@@ -107,4 +108,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+
 }
